2c8181debc7ea7797902de46c437a2c61e4778dd
Neelesh <NisumTech@nisumusersmbp8.gateway.pace.com>
2016-12-29 19:51:15 -0800

    modified job configuration and listeners

--- src/main/java/com/example/DemotaskApplication.java
+++ src/main/java/com/example/DemotaskApplication.java
@@ -1,14 +1,9 @@
 package com.example;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
-import org.springframework.context.annotation.Bean;
 
 @EnableTask
 @EnableBatchProcessing
@@ -19,10 +14,4 @@ public class DemotaskApplication {
 		SpringApplication.run(DemotaskApplication.class, args);
 	}
 	
-	@Bean
-   public CommandLineRunner commandLineRunner() {
-        return strings ->
-                System.out.println("Simple task application :Simple task application :Simple task application :Simple task application :" + 
-                      new SimpleDateFormat().format(new Date()));
-    }
 }
--- src/main/java/com/example/JobConfiguration.java
+++ src/main/java/com/example/JobConfiguration.java
@@ -8,16 +8,8 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.task.batch.listener.TaskBatchDao;
-import org.springframework.cloud.task.batch.listener.TaskBatchExecutionListener;
-import org.springframework.cloud.task.batch.listener.support.JdbcTaskBatchDao;
-import org.springframework.cloud.task.configuration.DefaultTaskConfigurer;
-import org.springframework.cloud.task.configuration.TaskConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JobConfiguration {
@@ -27,30 +19,6 @@ public class JobConfiguration {
  
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
-    
-    @Primary
-    @Bean(name="datasource1")
-    @ConfigurationProperties(prefix="spring.datasource")
-    public javax.sql.DataSource primaryDataSource() {
-        return  DataSourceBuilder.create().build();
-    }
-    
-    @Bean(name="datasource2")
-    @ConfigurationProperties(prefix="spring.datasource1")
-    public javax.sql.DataSource primaryDataSource1() {
-        return  DataSourceBuilder.create().build();
-    }
-    
-    @Bean
-    public TaskConfigurer taskConfigurer() {
-        return new DefaultTaskConfigurer(primaryDataSource());
-    }
-    
-    @Bean
-    public TaskBatchDao getBatchDao() {
-        return new JdbcTaskBatchDao(primaryDataSource());
-    }
-    
    
     @Bean
     public Job job() {
@@ -61,9 +29,7 @@ public class JobConfiguration {
               @Override
               public RepeatStatus execute(StepContribution contribution, 
                 ChunkContext chunkContext) throws Exception {
-                 
-               System.out.println("Job was run");
-               System.out.println("Job was run after sleep");
+                System.out.println("Job was run...........................................................");
                 return RepeatStatus.FINISHED;
               }
        }).build()).build();
